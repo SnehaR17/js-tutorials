9e0a37fa377ef87e30e876d48bcbcef8d274f8b8
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-04-22 18:10:18 +0000

    constant, variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 17845
+let accountEmail = "snehadrdn@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2   //not allowed to change a constant
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
