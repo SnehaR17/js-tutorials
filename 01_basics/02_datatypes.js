8cb3a20d522269d62c9c902cd5e5cb7a7f5d258b
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-04-22 18:56:19 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";   //treats all the js code as newer version
+
+//alert( 3 + 3) //this is node.js, not browser
+ 
+console.log(3 + 3);
+// code readability should be high
+
+console.log("Sneha"); 
+
+let name = "Sneha"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// primitive
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => declared but not initialised
+// symbol => used to find the uniqueness of the components
+
+//object
+
+console.log(typeof "Sneha");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
