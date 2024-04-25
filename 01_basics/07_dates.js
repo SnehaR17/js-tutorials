a6487441a1d0c7fc46fe6317f959d821ae3339db
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-04-25 18:23:16 +0000

    dates done

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,6 +1,8 @@
 // dates 
 
-const myDate = new Date()
+const myDate = new Date() // here we just made an instance of date object
+console.log(typeof myDate);
+console.log(myDate);
 console.log(typeof myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
@@ -16,3 +18,17 @@ console.log(myCreatedDate2.toLocaleString());
 //timeStamps => useful when u need the exact timestamps for eg. while conducting quizes or polls
 let myTimeStamp = Date.now()
 console.log(myTimeStamp); // gives long digits which is ntg but milliseconds from 01st jan 1970
+console.log(myCreatedDate.getTime()); // get time gives long digits as abpove from 1st jan 1970 till the myCreatedDate
+console.log(Math.floor(Date.now()/1000)); // converts into seconds, but we'll have decimal values so to avoid tht we can use math.flooer
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1); // jan is assigned with 0 so to to make it convinient to end users to understand we add 1 
+console.log(newDate.getDay()); // starts from monday i.e, 0
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+}) // this gives a similar result as noramal toLocaleString but this will help with more customization
