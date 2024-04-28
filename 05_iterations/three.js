a2704d20ea6a3b3547cd6d8065b347e40d32a7f8
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-04-28 18:46:01 +0000

    map done

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`)
+// }
+
+// // Maps
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India") // printed only once
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+// } // we get in array format, to avoid that we can follow the syntax below where we have to put [] around the iterator position
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+// const myObject = {
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// } // tells myOject is not iteratable // for of doesn't work for object // theres another way
