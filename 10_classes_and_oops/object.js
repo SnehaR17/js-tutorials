2596a807b4d9e0a126838c122e0c80c987d4f49f
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-05-11 12:18:28 +0000

    object done

--- 10_classes_and_oops/object.js
+++ 10_classes_and_oops/object.js
@@ -1,3 +1,47 @@
-function multipleBy5(
+// javascript's default behaviour is prototypal behaviour => if it doesn't get the thing it is seraching for then it won't immediately throw error instead it goes to the upper layers (like parents randparents) until it encounters null
+// prototype is the one that gives access to new keyword, class, this, prototypal inheritance etc
+
+
+// we can say that all the things in js are objects // so all the properties available with obj are available with array, strings etc (though all the properties are useable or not) 
+
+function multiplyBy5(num){
+    return num*5
+}
+multiplyBy5.power = 2
+
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.power)
+console.log(multiplyBy5.prototype)
+//with this we see that a fun is a fun also and if it needed it can behave like an object too
     
-)
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai", 25)
+const tea = createUser("tea", 250)
+
+chai.printMe()
+
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
