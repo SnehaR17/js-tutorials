c2f6cc9dfa37cdbc8f623325517985be8a59c93e
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-05-11 12:52:04 +0000

    Prototype done

---
+++ 10_classes_and_oops/prototype.js
@@ -0,0 +1,76 @@
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
+
+/*
+we can injectproperties from our end also
+we can access the highest hierarchy (ie. object) ang inject property into that, then that property will be available and can be accesed by string, arrays etc...
+so all the levels have the power as it is injected in the highest level
+
+wt if we give the the power to array (lower in hierarchy), then will the power be available with obj by default??
+NO. The obj wont get the power which is defined for array as the power can be passed from higher levels to lower levels but not vice versa
+*/
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
